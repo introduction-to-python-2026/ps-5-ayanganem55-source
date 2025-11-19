d8cea71fcf693ab439fd2651e4b8e1245e3bceea
ayanganem55-source <ayanganem55@gmail.com>
2025-11-19 21:48:34 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -2,10 +2,37 @@
 
 
 def split_before_uppercases(formula):
-    pass  # replace the pass with your code
+   parts = []
+    current_part = ""
+
+    for ch in formula:
+        if ch.isupper() and current_part:
+            parts.append(current_part)
+            current_part = ch
+        else:
+            current_part += ch
+
+  
+    if current_part:
+        parts.append(current_part)
+
+    return parts
+
 
 def split_at_digit(formula):
-    pass  # replace the pass with your code
+  digit_location = 1
+
+    for ch in formula[1:]:
+        if ch.isdigit():
+            break
+        digit_location += 1
+
+    if digit_location == len(formula):
+        return formula, 1
+
+    prefix = formula[:digit_location]
+    number = int(formula[digit_location:])
+    return prefix, number
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
